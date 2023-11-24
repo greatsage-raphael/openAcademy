db30a7d01f70d768936662cec55b4c41a41eb9fd
lubwama <bizzicole87@gmail.com>
2023-11-24 15:14:19 +0300

    Fix: navbar fixed

--- app/agents/page.tsx
+++ app/agents/page.tsx
@@ -4,17 +4,16 @@ export default function Home() {
   const InfoCard = (
     <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
       <h1 className="text-3xl md:text-4xl mb-4">
-        Download OpenAcademy ⬇️
+        Download OpenAcademy 
       </h1>
+      <button
+      type="submit"
+      className="shrink-0 px-8 py-4 bg-sky-600 rounded w-28">
+       Download
+      </button>
     </div>
   );
   return (
-    <ChatWindow
-      endpoint="api/chat"
-      emoji="🏴‍☠️"
-      titleText="Patchy the Chatty Pirate"
-      placeholder="I'm here to chat with you everything that you want to talk"
-      emptyStateComponent={InfoCard}
-    ></ChatWindow>
+    InfoCard
   );
 }
